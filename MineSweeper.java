5de051f9b36b1e83ea029dc2e65a254afc9b5a5f
lclc10 <51066801+lclc10@users.noreply.github.com>
2019-05-27 01:22:29 -0400

    places mines

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class MineSweeper
@@ -17,6 +18,29 @@ public class MineSweeper
 	public static void setUpBoard(int height, int width, int mines)
 	{
 		board = new String[height][width];
+		//fill board with 0's to initialize
+		for(int i=0; i<board.length; i++)
+			for(int j=0; j<board[0].length; j++)
+				board[i][j]=Integer.toString(0);
+				
+		//need to randomly generate the mines. How to get nonrepeating random numbers? https://stackoverflow.com/questions/8115722/generating-unique-random-numbers-in-java
+		
+		ArrayList<Integer> randlist = new ArrayList<Integer>();
+        for (int i=1; i<=height*width; i++) //for generating the mines each square of the board gets a number, starting a 1, running across rows then down columns. Number of squares is thus width*height
+        {
+            randlist.add(new Integer(i));
+        }
+        Collections.shuffle(randlist);
+        for (int i=0; i<mines; i++) 
+        {
+            System.out.println(randlist.get(i));
+            //mapping the generated mine location onto the board (had to figure out how to do)
+            int modresult = randlist.get(i)%width-1;
+            int y = (modresult == -1) ? width-1 : modresult;
+            int x = (modresult == -1) ? randlist.get(i)/width-1 : randlist.get(i)/width;
+            board[x][y]="M";
+        }
+			
 	}
 	
 	//print the board with row and column guides
